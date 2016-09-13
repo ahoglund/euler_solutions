42a40b6f3fca9006b26ebe7d90f70c7611a8c3be
Andrew Hoglund <hoglundandrew@ah-tunecore.local>
2016-09-12 20:06:29 -0400

    Some refactors

--- euler1.exs
+++ euler1.exs
@@ -1,25 +1,22 @@
 defmodule EulerOne do
-  # https://projecteuler.net/problem=1
-  #
-  # If we list all the natural numbers below 10 that are multiples
-  # of 3 or 5, we get 3, 5, 6 and 9.
-  # The sum of these multiples is 23.
+  def multiples(range), do: _multiples(Enum.to_list range)
 
-  Find the sum of all the multiples of 3 or 5 below 1000.
+  defp _multiples([]), do: 0
 
-  def multiples(range), do: _multiples((Enum.to_list range), 0)
+  defp _multiples([ head | tail ]) when rem(head, 3) == 0, do: head + _multiples(tail)
 
-  defp _multiples([], total), do: total
+  defp _multiples([ head | tail ]) when rem(head, 5) == 0, do: head + _multiples(tail)
 
-  defp _multiples([ head | tail ], total) when rem(head, 3) == 0 do
-   _multiples(tail, head + total)
+  defp _multiples([ _head | tail ]), do: _multiples(tail)
+
+  def multiples_of_three_and_five(range) do
+    range
+      |> Enum.to_list
+      |> _filter_by_three_or_five
+      |> Enum.sum
   end
 
-  defp _multiples([ head | tail ], total) when rem(head, 5) == 0 do
-   _multiples(tail, head + total)
-  end
-
-  defp _multiples([ head | tail ], total) do
-   _multiples(tail, total)
+  defp _filter_by_three_or_five(list) do
+    Enum.filter(list, &(rem(&1, 3) == 0 || rem(&1, 5) == 0))
   end
 end
